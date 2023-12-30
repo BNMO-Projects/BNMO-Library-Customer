c7f00fe7b8df20e04fa41d9356d75c7551a9900f
YakobusIP <yakobusiryantoprasethio@gmail.com>
2023-12-30 12:26:01 +0700

    feat: sidebar + topheader

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import { useAuthStore } from "@/store/auth.store";
+
+const routes = [
+  { name: "Home", path: "Home", icon: "/icons/home_solid.svg" },
+  { name: "Search", path: "Search", icon: "/icons/search_loop_outline.svg" },
+  { name: "My Library", path: "My Library", icon: "/icons/book_solid.svg" },
+  { name: "Settings", path: "Settings", icon: "/icons/cog_solid.svg" }
+];
+
+const authStore = useAuthStore();
+</script>
+
+<template>
+  <aside class="bg-yellow-mustard w-1/6 h-screen">
+    <div class="p-4 h-full">
+      <img src="/images/logo.webp" alt="BNMO Logo" class="w-32 mb-8" />
+      <div class="flex flex-col gap-2">
+        <RouterLink
+          v-for="route in routes"
+          :key="route.name"
+          :to="{ name: route.path }"
+          class="hover:bg-orange-coral p-2 rounded-lg"
+          :class="$route.name === route.name ? 'bg-orange-coral' : undefined"
+        >
+          <div class="flex items-center gap-3">
+            <img :src="route.icon" :alt="route.name" class="w-5" />
+            <p class="font-bold">{{ route.name }}</p>
+          </div>
+        </RouterLink>
+        <div
+          class="hover:bg-orange-coral p-2 rounded-lg hover:cursor-pointer"
+          @click="authStore.postLogout"
+        >
+          <div class="flex items-center gap-3">
+            <img
+              src="/icons/arrow_right_bracket.svg"
+              alt="Logout"
+              class="w-5"
+            />
+            <p class="font-bold">Logout</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </aside>
+</template>
---
+++ src/components/TopHeader.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { FwbAvatar } from "flowbite-vue";
+import { useAuthStore } from "@/store/auth.store";
+import { storeToRefs } from "pinia";
+
+const authStore = useAuthStore();
+const { getUsername } = storeToRefs(authStore);
+</script>
+
+<template>
+  <div
+    class="flex justify-end items-center gap-4 border-b border-b-black pb-4 px-4"
+  >
+    <p class="font-bold">
+      {{
+        new Date().toLocaleString("en-GB", {
+          weekday: "long",
+          day: "numeric",
+          month: "long",
+          year: "numeric"
+        })
+      }}
+    </p>
+    <div class="flex items-center justify-center gap-2">
+      <FwbAvatar rounded>
+        <template #placeholder>
+          <img src="/icons/user_solid.svg" alt="Avatar Icon" class="w-7" />
+        </template>
+      </FwbAvatar>
+      <p class="font-bold">{{ getUsername }}</p>
+    </div>
+  </div>
+</template>
