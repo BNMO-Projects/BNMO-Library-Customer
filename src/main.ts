823df3b9c3db4c90ea8a125fd29deab2bb02ca2b
YakobusIP <yakobusiryantoprasethio@gmail.com>
2023-12-30 10:00:13 +0700

    feat: auth store + vue-toast

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,20 @@ import { Component, createApp } from "vue";
 import App from "./App.vue";
 import "@/index.css";
 import { createPinia } from "pinia";
-import { router } from "@router/router";
+import { router } from "@/router/router";
+import axios from "axios";
+import Toast, { PluginOptions } from "vue-toastification";
 
 const pinia = createPinia();
+
+axios.defaults.baseURL = import.meta.env.VITE_BASE_APP_URL;
+
+const toastOptions: PluginOptions = {
+  timeout: 3000
+};
 
 const app = createApp(App as Component);
 app.use(pinia);
 app.use(router);
+app.use(Toast, toastOptions);
 app.mount("#app");
