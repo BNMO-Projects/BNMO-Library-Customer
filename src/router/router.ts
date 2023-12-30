05fb1b3416fb80cb7ffa5b026d7c5dcf1abd9bd9
YakobusIP <yakobusiryantoprasethio@gmail.com>
2023-12-30 12:26:10 +0700

    feat: search page template + routing

--- src/router/router.ts
+++ src/router/router.ts
@@ -4,9 +4,16 @@ import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 const Login = () => import("@/views/Login.vue");
 const Register = () => import("@/views/Register.vue");
 
+// Authenticated views
+const Search = () => import("@/views/Search.vue");
+
 const routes: RouteRecordRaw[] = [
   { path: "/login", name: "Login", component: Login },
-  { path: "/register", name: "Register", component: Register }
+  { path: "/register", name: "Register", component: Register },
+  { path: "/home", name: "Home", component: Search },
+  { path: "/search", name: "Search", component: Search },
+  { path: "/my-library", name: "My Library", component: Search },
+  { path: "/settings", name: "Settings", component: Search }
 ];
 
 export const router = createRouter({
---
+++ src/views/Search.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import TopHeader from "@/components/TopHeader.vue";
+import Sidebar from "@/components/Sidebar.vue";
+</script>
+
+<template>
+  <main class="flex min-h-screen">
+    <Sidebar />
+    <section class="bg-sky-blue w-5/6 flex flex-col p-8">
+      <TopHeader />
+    </section>
+  </main>
+</template>
