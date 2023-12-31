7de9392c984cd7a306f13d393b4253f4011afade
YakobusIP <yakobusiryantoprasethio@gmail.com>
2023-12-31 20:31:18 +0700

    style: responsive book detail

--- src/views/BookDetail.vue
+++ src/views/BookDetail.vue
@@ -21,25 +21,33 @@ onMounted(() => {
     <TopHeader />
     <div class="flex justify-center">
       <FwbSpinner v-if="isLoadingBookDetail" size="12" />
-      <div v-else class="flex flex-col w-full p-4 gap-8">
-        <div class="flex items-center gap-4">
+      <div v-else class="flex flex-col w-full p-0 lg:p-4 gap-8">
+        <div class="flex flex-col lg:flex-row items-center gap-4">
           <img
             :src="getBookDetail.book_cover"
             :alt="getBookDetail.id"
             class="w-48 rounded-md"
           />
           <div class="flex flex-col gap-4 w-full">
-            <div class="flex items-center justify-between gap-4">
-              <h1>{{ getBookDetail.title }}</h1>
+            <div
+              class="flex flex-col lg:flex-row items-center justify-between gap-4"
+            >
+              <h1 class="text-center">{{ getBookDetail.title }}</h1>
               <span class="bg-orange-coral px-4 py-2 rounded-lg font-bold">
                 {{ getBookDetail.book_type }}
               </span>
             </div>
-            <h3>{{ getBookDetail.author_name }}</h3>
-            <p>ISBN: {{ getBookDetail.isbn }}</p>
-            <div class="flex gap-4 border-t border-t-black pt-4 items-center">
+            <h3 class="text-center lg:text-left">
+              {{ getBookDetail.author_name }}
+            </h3>
+            <p class="text-center lg:text-left">
+              ISBN: {{ getBookDetail.isbn }}
+            </p>
+            <div
+              class="flex flex-col lg:flex-row gap-4 border-t border-t-black pt-4 items-center"
+            >
               <button
-                class="bg-yellow-mustard hover:bg-orange-coral transition ease-in-out text-base font-bold rounded-lg px-4 py-2 flex items-center justify-center gap-2"
+                class="bg-yellow-mustard hover:bg-orange-coral transition ease-in-out w-full lg:w-fit text-base font-bold rounded-lg px-4 py-2 flex items-center justify-center gap-2"
               >
                 <img
                   src="/icons/heart_solid.svg"
@@ -49,7 +57,7 @@ onMounted(() => {
                 Add to wishlist
               </button>
               <button
-                class="bg-yellow-mustard hover:bg-orange-coral transition ease-in-out text-base font-bold rounded-lg px-4 py-2 flex items-center justify-center gap-2"
+                class="bg-yellow-mustard hover:bg-orange-coral transition ease-in-out w-full lg:w-fit text-base font-bold rounded-lg px-4 py-2 flex items-center justify-center gap-2"
               >
                 <img
                   src="/icons/cart_plus_solid.svg"
@@ -73,25 +81,21 @@ onMounted(() => {
                 {{ getBookDetail.original_stock }}
               </p>
             </div>
-            <div class="flex gap-4">
-              <div class="flex items-center gap-2">
-                <p class="font-bold">Category:</p>
+            <div class="flex flex-col flex-wrap lg:flex-row gap-4 items-center">
+              <div class="flex gap-4 items-center">
                 <span class="bg-orange-coral text-sm px-2 rounded-md">
                   {{ getBookDetail.category_name }}
                 </span>
-              </div>
-              <div class="flex items-center gap-2">
-                <p class="font-bold">Genre:</p>
+
                 <span class="bg-orange-coral text-sm px-2 rounded-md">
                   {{ getBookDetail.genre_name }}
                 </span>
-              </div>
-              <div class="flex items-center gap-2">
-                <p class="font-bold">Language:</p>
+
                 <span class="bg-orange-coral text-sm px-2 rounded-md">
                   {{ getBookDetail.language_name }}
                 </span>
               </div>
+
               <p class="font-bold">
                 Publication:
                 {{ new Date(getBookDetail.publication_year).getFullYear() }}
@@ -99,7 +103,7 @@ onMounted(() => {
             </div>
           </div>
         </div>
-        <div class="flex flex-col w-full gap-4">
+        <div class="flex flex-col w-full gap-4 mb-12 lg:mb-0">
           <h3>Description</h3>
           <p>{{ getBookDetail.description }}</p>
         </div>
