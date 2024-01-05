a5ba29d363ddc883adaefb518cc835be09e9f65e
YakobusIP <yakobusiryantoprasethio@gmail.com>
2024-01-05 22:04:44 +0700

    feat: new color scheme

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,9 +9,20 @@ export default {
   theme: {
     extend: {
       colors: {
-        "yellow-mustard": "#FFC107",
-        "orange-coral": "#FF7F50",
-        "sky-blue": "#87CEEB"
+        "text-color": "#2F4F4F",
+        "main-color": "#F5F5DC",
+        "secondary-color": "#34568B",
+        "hover-color": "#5B7DB1",
+        "tag-color": "#97BC62",
+        "container-color": "#EBEBEB",
+        "extra-color": "#FFA07A",
+        "text-color-dark": "#E0E0E0",
+        "main-color-dark": "#121212",
+        "secondary-color-dark": "#1E3A5F",
+        "hover-color-dark": "#2B5178",
+        "tag-color-dark": "#748B47",
+        "container-color-dark": "#1E1E1E",
+        "extra-color-dark": "#CC7B66"
       },
       screens: {
         "3xl": "1920px"
