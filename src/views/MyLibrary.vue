ef85d1b02ad324afb3ac7c6faa914a4b1415260d
YakobusIP <yakobusiryantoprasethio@gmail.com>
2024-01-08 19:30:20 +0700

    feat: add new order history tab

--- src/views/MyLibrary.vue
+++ src/views/MyLibrary.vue
@@ -2,10 +2,17 @@
 import { ref } from "vue";
 import LoggedLayout from "@/components/global/LoggedLayout.vue";
 import TopHeader from "@/components/global/TopHeader.vue";
-import Wishlist from "@/components/my-library/Wishlist.vue";
+import Wishlist from "@/components/my-library/wishlist/Wishlist.vue";
+import OrderHistory from "@/components/my-library/order-history/OrderHistory.vue";
+import { router } from "@/router/router";
 
 const tabs = [
   { id: "wishlist", name: "Wishlist", query: { page: 1 } },
+  {
+    id: "order-history",
+    name: "Order History",
+    path: "/order-history"
+  },
   {
     id: "currently-borrowed",
     name: "Currently Borrowed",
@@ -14,7 +21,7 @@ const tabs = [
 ];
 
 const isLg = ref(window.screen.width > 1023);
-const activeTab = ref(tabs[0].id);
+const activeTab = ref(router.currentRoute.value.params.tab as string);
 </script>
 
 <template>
@@ -24,7 +31,7 @@ const activeTab = ref(tabs[0].id);
       class="flex flex-col flex-1 text-text-color dark:text-text-color-dark gap-4"
     >
       <ul
-        class="flex flex-wrap text-base font-bold text-center text-gray-500 dark:text-gray-400"
+        class="flex whitespace-nowrap overflow-auto text-base font-bold text-center text-gray-500 dark:text-gray-400"
       >
         <li class="me-2" v-for="tab in tabs" :key="tab.id">
           <RouterLink
@@ -45,6 +52,7 @@ const activeTab = ref(tabs[0].id);
         </li>
       </ul>
       <Wishlist v-if="activeTab === 'wishlist'" :is-lg="isLg" />
+      <OrderHistory v-if="activeTab === 'order-history'" :is-lg="isLg" />
     </div>
   </LoggedLayout>
 </template>
